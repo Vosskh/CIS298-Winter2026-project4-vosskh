3aa72c3ceeb9e783ed7775b3cf3f0612c2922f8c
Eric Charnesky <2469199+EricCharnesky@users.noreply.github.com>
2026-03-11 11:41:27 -0400

    registration

--- Week9-Databases/main.py
+++ Week9-Databases/main.py
@@ -2,12 +2,42 @@
 
 import sqlite3
 
-conn = sqlite3.connect('music.sqlite')
-cur = conn.cursor()
+connection = sqlite3.connect('registration.sqlite')
+cursor = connection.cursor()
 
-# cur.execute('DROP TABLE IF EXISTS Track')
-# cur.execute('CREATE TABLE Track (title TEXT, plays INTEGER)')
+choice = ""
 
-cur.execute("INSERT INTO artist (name, eyes) VALUES ('Frank Sinatra', 'blue')")
-conn.commit()
-conn.close()
+while choice != "QUIT":
+
+    choice = input("Enter a choice: 1 - Manage Faculty")
+
+    if choice == "1":
+        action = input("Enter 1 for List Faculty, 2 for Add Faculty, 3 for Update Faculty")
+
+        if action == "1":
+            cursor.execute('SELECT * FROM Faculty')
+            print("id, name, email")
+            for row in cursor:
+                print(row)
+        elif action == "2":
+            name = input("Enter name")
+            email = input("Enter email")
+            cursor.execute('INSERT INTO faculty (name, email) VALUES (?, ?)',(name, email))
+            connection.commit()
+        elif action == "3":
+            id = int(input("Enter the ID to update"))
+            name = input("Enter name")
+            email = input("Enter email")
+            cursor.execute('update faculty set name = ?, email = ? WHERE id = ?', (name, email, id) )
+            connection.commit()
+
+
+# to get a transcript for a given student
+# select course.Department, Course.Number, course.Credits, Enrollment.Grade from Enrollment
+# inner join student on Student.id = Enrollment.Student_ID
+# inner join section on Section.ID = Enrollment.Section_ID
+# inner join course on Course.id = Section.Course_ID
+# where Student_ID = 1
+
+
+connection.close()
